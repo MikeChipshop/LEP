012630acb10c99483f75a7bb1510ffcee7b7d81a
Mike Bishop <mike@miniman-webdesign.co.uk>
2018-07-28 14:06:31 +0100

    Edited insta template layout, removed list

--- js/site-common.js
+++ js/site-common.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function( $ ) {
 		accessToken: '4082314604.1677ed0.05fa56beec6a4fc6953f617effc1b010',
 		client_id: '10b65a7b8c3c4d3e883e142822c59d1d',
 		limit: 8,
-		template: '<li><a href="{{link}}"><img src="{{image}}" alt="{{caption}}"/></a></li>',
+		template: '<a href="{{link}}"><img src="{{image}}" alt="{{caption}}"/></a>',
 		resolution: 'thumbnail',
 	});
 	feed.run();
