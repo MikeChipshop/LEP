c820fc4e757e260d8a3922afbfc1805a134e47fe
Mike Bishop <mike@miniman-webdesign.co.uk>
2018-07-28 12:10:23 +0100

    Added custom nav to main nav

--- header.php
+++ header.php
@@ -17,12 +17,7 @@
 				Logo
 			</div>
 			<nav class="lep_global-nav">
-				<ul>
-					<li><a href="#">Portfolio</a></li>
-					<li><a href="#">Blog</a></li>
-					<li><a href="#">Services</a></li>
-					<li><a href="#">Contact</a></li>
-				</ul>
+				<ul><?php wp_nav_menu( array('theme_location' => 'main_menu' )); ?></ul>
 			</nav>
 		</div>
 	</header>
