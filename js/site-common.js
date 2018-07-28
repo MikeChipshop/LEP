11090d582ffa36965ecabc3dc4183714c59cd261
Mike Bishop <mike@miniman-webdesign.co.uk>
2018-07-28 14:07:14 +0100

    changed alt text on instagram images

--- js/site-common.js
+++ js/site-common.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function( $ ) {
 		accessToken: '4082314604.1677ed0.05fa56beec6a4fc6953f617effc1b010',
 		client_id: '10b65a7b8c3c4d3e883e142822c59d1d',
 		limit: 8,
-		template: '<a href="{{link}}"><img src="{{image}}" alt="{{caption}}"/></a>',
+		template: '<a href="{{link}}"><img src="{{image}}" alt="Visit Little Elk Photography on Instagram"/></a>',
 		resolution: 'thumbnail',
 	});
 	feed.run();
