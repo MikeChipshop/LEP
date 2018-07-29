45fa6dcf1f12e51554cb3df0a479a5fe7669e4ec
Mike Bishop <mike@miniman-webdesign.co.uk>
2018-07-29 16:00:15 +0100

    Basic layout for archives

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
 <?php get_header(); ?>
+<main>
+	<section class="lep_blog-landing-wrap">
+        <div class="lep_main-wrap">
+            <?php if ( have_posts() ) : ?>
+                <ul>
+                    <?php while ( have_posts() ) : the_post(); ?>
+                        <li>
+                            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                                <a href="<?php the_permalink(); ?>" title="Read <?php the_title(); ?>">
+                                    <img src="#">
+                                    <div class="lep_archive-landing-item">
+                                        <h1><?php the_title(); ?></h1>
+                                    </div>
+                                </a>
+                            </article>
+                        </li>
+                    <?php endwhile; ?>
+                </ul>
+                <nav class="lep_pagination">
+					<?php echo paginate_links(); ?>
+				</nav>
+            <?php else: ?>
+                <div class="lep_no-content">
+                    <h1>Apologies, no content found</h1>
+                </div>
+            <?php endif; ?>
+        </div>
+    </section>
 
+	<section class="lep_fp-insta">
+		<div id="instafeed"></div>
+	</section>
+</main>
 <?php get_footer(); ?>
