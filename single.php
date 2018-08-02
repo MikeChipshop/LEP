e9fd6cdb82bf65213b80ac8f971e48bce0c51148
Mike Bishop <mike@miniman-webdesign.co.uk>
2018-08-02 11:31:09 +0100

    RTE styles and Meta info placeholders

--- single.php
+++ single.php
@@ -3,12 +3,16 @@
 <?php if ( have_posts() ) : ?>
     <?php while ( have_posts() ) : the_post();?>
 
-        <div class="lep_single-blog-wrap">
-            <h2><?php the_title(); ?></h2>
+        <article class="lep_single-blog-wrap">
+            <h2 class="lep_single-blog-title"><?php the_title(); ?></h2>
+            <div class="lep_single-blog-meta">
+                <div class="lep_single-blog-date"><?php the_time(); ?></div>
+                <div class="lep_single-blog-cat"><a href="#">Photography</a>, <a href="#">Services</a>, <a href="#">Thoughts</a></div>
+            </div>
             <div class="rte">
                 <?php the_content(); ?>
             </div>
-        </div>
+        </article>
 
     <?php endwhile; ?>
 
