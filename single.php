6f71c04332b75b5c31773b51b28272783ecfe31f
Mike Bishop <mike@miniman-webdesign.co.uk>
2018-08-02 11:02:54 +0100

    Basic single blog structure in place

--- single.php
+++ single.php
@@ -1,3 +1,22 @@
 <?php get_header(); ?>
 
+<?php if ( have_posts() ) : ?>
+    <?php while ( have_posts() ) : the_post();?>
+
+        <div class="lep_single-blog-wrap">
+            <h2><?php the_title(); ?></h2>
+            <div class="rte">
+                <?php the_content(); ?>
+            </div>
+        </div>
+
+    <?php endwhile; ?>
+
+    <div class="lep_single-blog-no-content">
+        <h2>Oops</h2>
+        <h3>No content here, return to the home page and try again</h3>
+    </div>
+
+<?php endif; ?>
+
 <?php get_footer(); ?>
